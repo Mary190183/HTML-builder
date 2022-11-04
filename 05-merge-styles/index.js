24f7cbb7dce2a1e1ca55d00e6343d0e48486acce
Mariya Petrovskaya <pma190183@yandex.ru>
2022-11-05 01:35:34 +0300

    feat: 05 add

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const source = path.resolve(__dirname, 'styles');
+const destination = path.resolve(__dirname, 'project-dist');
+
+const output = fs.createWriteStream(path.resolve(destination, 'bundle.css'), 'utf-8');
+
+fs.readdir(source, function (err, files) {
+  files.forEach(file => {
+    let fileInfo = path.resolve(source, file);
+    const {ext} = path.parse(fileInfo);
+
+    if (ext.slice(1) === 'css') {
+      let stream = fs.createReadStream(fileInfo, 'utf-8');
+
+      stream.on('data', chunk => output.write(chunk), (err) => {
+        if (err) {
+          console.error(err)
+          return
+          }})
+    } 
+  });
+});
