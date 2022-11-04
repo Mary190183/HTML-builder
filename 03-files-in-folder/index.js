5e03f230e06111333fe9ed7dab15d7ebb5f4be66
Mariya Petrovskaya <pma190183@yandex.ru>
2022-11-04 23:12:02 +0300

    feat: 03 add

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const secretFolder = path.join(__dirname, "secret-folder");
+const {stdout} = process;
+
+fs.readdir(secretFolder, function (err, files) {
+  files.forEach(file => {
+    let fileInfo = path.join(secretFolder, file);
+    
+    fs.stat(fileInfo, (err, stats) => {
+      if (stats.size > 0) {
+        const {name, ext} = path.parse(fileInfo);
+        let output = `${name} - ${ext.slice(1)} - ${stats.size} bytes\n`;
+        stdout.write(output)
+      }
+    });
+  });
+}
+);
