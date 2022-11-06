976eb7d599f4a7b1a1a6d3de36cda23880a0de78
Mariya Petrovskaya <pma190183@yandex.ru>
2022-11-06 23:23:51 +0300

    feat: add 06

---
+++ 06-build-page/index.js
@@ -0,0 +1,118 @@
+const fs = require('fs');
+const path = require('path');
+const destination = path.resolve(__dirname, 'project-dist');
+//создание папки project-dist
+async function mkdir(directory) {
+  fs.mkdir(directory, {recursive: true }, (err) => {   
+    if (err) {
+    console.error(err)
+    return
+    }
+  });
+}
+
+mkdir(destination);
+
+//создание styles
+const source = path.resolve(__dirname, 'styles');
+const output = fs.createWriteStream(path.resolve(destination, 'style.css'), 'utf-8');
+
+fs.readdir(source, function (err, files) {
+  files.forEach(file => {
+    let fileInfo = path.resolve(source, file);
+    const {ext} = path.parse(fileInfo);
+
+    if (ext.slice(1) === 'css') {
+      let stream = fs.createReadStream(fileInfo, 'utf-8');
+
+      stream.on('data', chunk => output.write(chunk), (err) => {
+        if (err) {
+          console.error(err)
+          return
+          }})
+    } 
+  });
+});
+
+// копирование assets
+let sourceFonts = path.resolve( __dirname, 'assets', 'fonts');
+let sourceImg = path.resolve( __dirname, 'assets', 'img');
+let sourceSvg = path.resolve( __dirname, 'assets', 'svg');
+let destinationCopy= path.resolve( __dirname, 'project-dist/assets');
+let destinationFonts = path.resolve( __dirname, 'project-dist/assets/fonts');
+let destinationImg = path.resolve( __dirname, 'project-dist/assets/img');
+let destinationSvg = path.resolve( __dirname, 'project-dist/assets/svg');
+
+//создание папок
+mkdir(destinationCopy);
+mkdir(destinationFonts);
+mkdir(destinationImg);
+mkdir(destinationSvg);
+//очистка 
+async function unlink(directory) {
+  files => {
+files.forEach(file => {
+  fs.unlink(path.resolve(directory, file), (err) => {
+    if (err) {
+    console.error(err)
+    return
+    }
+  });
+});
+}
+}
+unlink(destinationFonts);
+unlink(destinationImg);
+unlink(destinationSvg);
+//копирование
+async function copyDir(directory, src) {
+  fs.readdir(src, function (err, files) {
+    files.forEach(file => {
+      fs.copyFile(path.resolve(src, file), path.resolve(directory, file), (err) => {
+        if (err) {
+          console.error(err)
+          return
+      }
+      });
+    }); 
+  });
+}
+copyDir(destinationImg, sourceImg)
+copyDir(destinationSvg, sourceSvg)
+copyDir(destinationFonts, sourceFonts)
+
+//создание index.html
+const components = path.resolve(__dirname, 'components');
+const outputHTML = fs.createWriteStream(path.resolve(destination, 'index.html'), 'utf-8');
+const componentsNew = new Object;
+fs.readdir(components, function (err, files) {
+  
+  files.forEach(file => {
+    let stream = fs.createReadStream(path.resolve(components, file), 'utf-8');
+    stream.on('data', chunk => {
+      let fileInfo = path.resolve(components, file);
+      const {ext} = path.parse(fileInfo);
+      componentsNew[path.basename(file, ext)] = chunk;
+      
+    }), (err) => {
+      if (err) {
+        console.error(err)
+        return
+    }}
+  })
+
+  let streamTemplate = fs.createReadStream(path.resolve(__dirname, 'template.html'), 'utf-8');
+
+  streamTemplate.on('data', tmp => {
+    let result = '';
+    
+    while(tmp.search('{{') > 0 && tmp.search('}}') > tmp.search('{{')) {
+      outputHTML.write(tmp.substring(0, tmp.search('{{')));
+      result = tmp.substring(tmp.search('{{')+2, tmp.search('}}'));
+      outputHTML.write(String(componentsNew[result]));
+      tmp = tmp.substring(tmp.search('}}')+2, tmp.length);
+    }
+    outputHTML.write(tmp);
+  });
+});
+
